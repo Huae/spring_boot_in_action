eccc862eebabbdafdbab95094b77f43273b69721
Huae <huanghe2r@foxmail.com>
2020-03-23 23:48:03 +0800

    ch4 finished springmvc basic

---
+++ ch4_springmvc/src/main/java/com/huae/ch4/springmvc/WebInitializer.java
@@ -0,0 +1,29 @@
+package com.huae.ch4.springmvc;
+
+import com.huae.ch4.springmvc.config.SpringMVCconfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Spring使用WebApplicationInitializer来配置Servlet3.0+的接口,从而替代web.xml
+ * 实现WebApplicationInitializer接口的类会被Spring容器获取到
+ */
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 初始化WebApplicationContext 注册配置类并将其和当前ServletContext关联
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(SpringMVCconfig.class);
+        ctx.setServletContext(servletContext);
+
+        // 配置springmvc核心Servlet(请求拦截分发)
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
